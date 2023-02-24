82a4502ba5a2bc854e70793eab87f00dcac862de
lorenzo rivera <lorenzo23rivera@gmail.com>
2023-02-23 18:12:59 -0800

    Add O,O,P code and self eval

--- oop.rb
+++ oop.rb
@@ -2,7 +2,16 @@
 # it should have a dynamic name attribute
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
-
+class Unicorn
+    def initialize(name)
+      @name = name
+      @color = 'silver'
+    end
+  
+    def say(word)
+      p "*~*#{word}*~*"
+    end
+  end
 
 
 #  Write a class called Vampire
@@ -10,7 +19,17 @@
 #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
-
+class Vampire
+    def initialize(name, pet = 'bat')
+      @name = name
+      @pet = pet
+      @thirsty = true
+    end
+  
+    def drink
+      @thirsty = false
+    end
+  end
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -18,7 +37,20 @@
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
-
+class Dragon
+    def initialize(name, rider, color)
+      @name = name
+      @rider = rider
+      @color = color
+      @is_hungry = true
+      @num_eats = 0
+    end
+  
+    def eat
+      @num_eats += 1
+      @ishungry = false if @num_eats >= 4
+    end
+  end
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -28,3 +60,36 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+class Hobbit
+    def initialize(name, disposition, age = 0)
+      @name = name
+      @disposition = disposition
+      @age = age
+      @is_old = false
+      @is_old = @age >= 101
+      @is_adult = false
+      @is_adult = @age >= 33
+  
+      @has_ring = @name == 'Frodo'
+    end
+  
+    def celebrate_birthday
+      @age += 1
+    end
+  endclass Hobbit
+  def initialize(name, disposition, age = 0)
+    @name = name
+    @disposition = disposition
+    @age = age
+    @is_old = false
+    @is_old = @age >= 101
+    @is_adult = false
+    @is_adult = @age >= 33
+
+    @has_ring = @name == 'Frodo'
+  end
+
+  def celebrate_birthday
+    @age += 1
+  end
+end
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,5 @@
 # Self-Evaluation
 
-- This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
-- How is your workflow going, in terms of taking breaks, staying focused, etc.?
-- What did you learn from this exercise that you want to remember as you begin Mod 1?
+- This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges? I feel like with a group i was able to rely on everyone while still adding my own ideas so i think i faced these challenges well.
+- How is your workflow going, in terms of taking breaks, staying focused, etc.? It is still a bit rough but it is getting better by the day.
+- What did you learn from this exercise that you want to remember as you begin Mod 1? Honestly i want to remember almost everything i learnd about O,O,P  so that i can use it effectivly in the future
